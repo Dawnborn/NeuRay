2e14160485e65fd98671f035db5f3379ed3383a0
junpeng <junpeng.hu@tum.de>
2023-09-26 17:10:29 +0200

    update gitignore and prep_cuda

--- prep_cuda.sh
+++ prep_cuda.sh
@@ -1,8 +1,8 @@
 # conda activate grasp37cu113
 CUDA_VERSION=11.1
 
-export CUDA_HOME=/home/junpeng.hu/Documents/ws_graspnerf/mycudatoolkit/cuda-$CUDA_VERSION
+export CUDA_HOME=/data/hdd1/storage/junpeng/ws_graspnerf/mycudatoolkit/cuda-$CUDA_VERSION
 
-export PATH=/home/junpeng.hu/Documents/ws_graspnerf/mycudatoolkit/cuda-${CUDA_VERSION}/bin:$PATH
+export PATH=/data/hdd1/storage/junpeng/ws_graspnerf/mycudatoolkit/cuda-${CUDA_VERSION}/bin:$PATH
 
-export LD_LIBRARY_PATH=/home/junpeng.hu/Documents/ws_graspnerf/mycudatoolkit/cuda-${CUDA_VERSION}/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/data/hdd1/storage/junpeng/ws_graspnerf/mycudatoolkit/cuda-${CUDA_VERSION}/lib64:$LD_LIBRARY_PATH
